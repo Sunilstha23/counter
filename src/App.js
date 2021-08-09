9256a47b99ad2d14103239f00ec9fb7d9c308d47
Sunil Shrestha <sunilshresthamail@gmail.com>
2021-08-09 14:26:52 +0545

    added counter( increment decrement and reset)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const { useState } = React;
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        // if else statement to determine color of the counter
+      }
+      <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>
+        {count}
+      </h1>
+      <div className="button__wrapper">
+        <button onClick={() => setCount(count - 1)}><i class="fa fa-minus" aria-hidden="true"></i></button>
+        <button onClick={() => setCount(0)}><i class="fa fa-refresh" aria-hidden="true"></i></button>
+        <button onClick={() => setCount(count + 1)}><i class="fa fa-plus" aria-hidden="true"></i></button>
+      </div>
     </div>
   );
 }
